bb01c63bd9082ee219476f8a9b6038b5cdbac2d0
Courtney Schirf <courtney.schirf@gmail.com>
2017-12-20 01:05:16 -0600

    Removing unused files and importing default GDI styles. Also, turning off some scss linter rules.

--- static/js/webslides.js
+++ static/js/webslides.js
@@ -1,7 +1,7 @@
 /*!
  * Name: WebSlides
  * Version: 1.5.0
- * Date: 2017-09-16
+ * Date: 2017-12-20
  * Description: Making HTML presentations easy
  * URL: https://github.com/webslides/webslides#readme
  * Credits: @jlantunez, @LuisSacristan, @Belelros
