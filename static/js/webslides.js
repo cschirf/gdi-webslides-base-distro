d88f453ba211c44767b7ea2625cc0d8db305b67d
cschirf <courtney.schirf@gmail.com>
2018-02-27 21:17:37 -0600

    updating a handful of styles

--- static/js/webslides.js
+++ static/js/webslides.js
@@ -1,7 +1,7 @@
 /*!
  * Name: WebSlides
  * Version: 1.5.0
- * Date: 2017-12-20
+ * Date: 2018-02-28
  * Description: Making HTML presentations easy
  * URL: https://github.com/webslides/webslides#readme
  * Credits: @jlantunez, @LuisSacristan, @Belelros
